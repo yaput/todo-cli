0f4729a0e980a41c4a95bce2da678cfbd982cede
yaput <antonius.putra@traveloka.com>
2024-07-22 11:51:50 +0700

    Use os env

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 )
 
 func main() {
+	email := os.Getenv("JIRA_EMAIL")
 	token := os.Getenv("JIRA_TOKEN")
-	jclient := storage_api.NewJiraClient("https://29022131.atlassian.net", "antonius.putra@traveloka.com", token, "847")
+	jiraBoardID := os.Getenv("JIRA_BOARD_ID")
+	jiraUrl := os.Getenv("JIRA_URL")
+	jclient := storage_api.NewJiraClient(jiraUrl, email, token, jiraBoardID)
 	todoList := interface_api.NewInterface(*storage_api.NewStorage(*jclient))
 
 	todoList.StartTodoList()
